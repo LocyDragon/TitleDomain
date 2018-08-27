594bc50ad9243bd935dc88b51ed4fbe152654742
LocyDragon <2424441676@qq.com>
2018-08-27 21:31:49 +0800

    init

---
+++ src/main/java/com/locydragon/td/listeners/ThreadLoadListener.java
@@ -0,0 +1,12 @@
+package com.locydragon.td.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class ThreadLoadListener implements Listener {
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+
+	}
+}
